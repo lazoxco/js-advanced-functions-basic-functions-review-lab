89a2c698e6c24aaddb1f788215fabb922beac353
github username <github email address>
2021-04-21 19:59:59 -0400

    commit tree 9020d5dec014a0fee4aa3b3c4c92d20b4c9aec2b

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Your code here
+function saturdayFun(activity="roller-skate") {
+  return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(work="go to the office") {
+  return `This Monday, I will ${work}.`
+}
+
+function wrapAdjective(star="*"){
+  return function(adj="special") {
+    return `You are ${star}${adj}${star}!`
+  }
+}
+
+const Calculator = {
+  add: function(a, b) {
+    return a + b
+  },
+  subtract: (a, b) => {
+    return a - b
+  },
+  multiply: (a, b) => {
+    return a * b
+  },
+  divide: (a, b) => {
+    return a / b
+  }
+}
+
+function actionApplyer(start, functions) {
+  for (let i = 0; i < functions.length; i++) {
+    start = functions[i](start)
+  }
+  return start;
+}
